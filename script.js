34f7e79a770bce1b10c540edf7d09c6708c4e319
Aureen <yukiansff@gmail.com>
2020-09-28 22:22:08 +0200

    All checks work as intended.

--- script.js
+++ script.js
@@ -8,16 +8,26 @@ const errorContent = (() => {
     missing: 'Please enter a zip code',
     invalid: 'Please enter a valid Zip Code (can only contain numbers and letters)'
   }
+  const password = {
+    missing: 'Please enter a password',
+    invalid: 'Please make sure your password meets the following conditions.'
+  }
   const confirmPassword = {
     missing: 'Please confirm your password',
-    invalid: 'Your passwords don\'t seem to match, could you check them again please?'
+    invalid: 'Your passwords don\'t seem to match, could you please check them again?'
+  }
+  const submit = {
+    valid: 'Thank you for your participation!',
+    invalid: 'A few fields seem invalid, could you please check them again?'
   }
 
   return {
     name,
     email,
     zipCode,
-    confirmPassword
+    password,
+    confirmPassword,
+    submit
   }
 })()
 
@@ -62,58 +72,91 @@ const checkValidity = (() => {
     name: document.querySelector('#error-name'),
     email: document.querySelector('#error-email'),
     zip: document.querySelector('#error-zip'),
-    confirm: document.querySelector('#error-confirm')
+    password: document.querySelector('#error-password'),
+    confirm: document.querySelector('#error-confirm'),
+    submit: document.querySelector('#error-submit')
   }
+
+  const _submit = document.querySelector('#submit')
 
   const _checkMissing = (input, errorElem, error) => {
     if (input.validity.valueMissing) {
       displayError.error(input, errorElem, error)
-    } else {
-      displayError.noError(input, errorElem)
     }
   }
 
   const _checkType = (input, errorElem, error) => {
     if (input.validity.typeMismatch) {
       displayError.error(input, errorElem, error)
-    } else {
-      displayError.noError(input, errorElem)
     }
   }
 
   const _checkPattern = (input, errorElem, error) => {
     if (input.validity.patternMismatch) {
       displayError.error(input, errorElem, error)
-    } else {
-      displayError.noError(input, errorElem)
     }
   }
 
-  const _checkIdentical = (input1, input2, errorElem, error) => {
+  const _checkValidity = (input, errorElem) => {
+    if (input.checkValidity()) displayError.noError(input, errorElem)
+  }
+
+  const _checkIdentical = (input1, input2) => {
     if (input1.value !== input2.value) {
-      displayError.error(input2, errorElem, error)
-    } else {
-      displayError.noError(input2, errorElem)
+      return false
     }
+    return true
   }
 
   const _checkName = () => {
     _checkMissing(_inputs.name, _errors.name, errorContent.name)
+    _checkValidity(_inputs.name, _errors.name)
   }
 
   const _checkMail = () => {
-    _checkType(_inputs.email, _errors.email, errorContent.email.invalid)
     _checkMissing(_inputs.email, _errors.email, errorContent.email.missing)
+    _checkType(_inputs.email, _errors.email, errorContent.email.invalid)
+    _checkValidity(_inputs.email, _errors.email)
   }
 
   const _checkZip = () => {
-    _checkPattern(_inputs.zip, _errors.zip, errorContent.zipCode.invalid)
     _checkMissing(_inputs.zip, _errors.zip, errorContent.zipCode.missing)
+    _checkPattern(_inputs.zip, _errors.zip, errorContent.zipCode.invalid)
+    _checkValidity(_inputs.zip, _errors.zip)
+  }
+
+  const _checkPassword = () => {
+    _checkMissing(_inputs.password, _errors.password, errorContent.password.missing)
+    _checkPattern(_inputs.password, _errors.password, errorContent.password.invalid)
   }
 
   const _checkConfirmation = () => {
-    _checkIdentical(_inputs.password, _inputs.confirm, _errors.confirm, errorContent.confirmPassword.invalid)
-    _checkMissing(_inputs.confirm, _errors.confirm, errorContent.confirmPassword.missing)
+    if (_inputs.confirm.validity.valueMissing) {
+      displayError.error(_inputs.confirm, _errors.confirm, errorContent.confirmPassword.missing)
+    } else if (!_checkIdentical(_inputs.password, _inputs.confirm)) {
+      displayError.error(_inputs.confirm, _errors.confirm, errorContent.confirmPassword.invalid)
+    } else {
+      displayError.noError(_inputs.confirm, _errors.confirm)
+    }
+  }
+
+  const _checkSubmit = (e) => {
+    e.preventDefault()
+    for (const key in _inputs) {
+      console.log(_inputs[key].checkValidity())
+      console.log(_checkIdentical(_inputs.password, _inputs.confirm))
+      if (!_inputs[key].checkValidity() || !_checkIdentical(_inputs.password, _inputs.confirm)) {
+        displayError.error(_submit, _errors.submit, errorContent.submit.invalid)
+        _checkName()
+        _checkMail()
+        _checkZip()
+        _checkPassword()
+        _checkConfirmation()
+      } else {
+        displayError.error(_submit, _errors.submit, errorContent.submit.valid)
+        displayError.validMessage(_errors.submit)
+      }
+    }
   }
 
   const enableChecks = () => {
@@ -121,6 +164,8 @@ const checkValidity = (() => {
     _inputs.email.addEventListener('blur', _checkMail)
     _inputs.zip.addEventListener('blur', _checkZip)
     _inputs.confirm.addEventListener('blur', _checkConfirmation)
+    _inputs.password.addEventListener('blur', _checkPassword)
+    _submit.addEventListener('click', _checkSubmit)
   }
 
   return { enableChecks }
